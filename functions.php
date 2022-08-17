45b7fef8f3ae1b864eedcf5466e07ac96e663dbd
Krishna Hawk <krishnahawk@Krishnas-MacBook-Pro.local>
2022-08-17 01:19:00 -1000

    Added XMLRPC disable hook to functions.php to prevent exploit hacks.

--- functions.php
+++ functions.php
@@ -3,3 +3,6 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+/* Turn off XMLRPC right off the bat to prevent exploit hacks */
+add_filter( ‘xmlrpc_enabled’, ’_return_false’ );
